045b6ee6e6cc5aa98e95d66aee0797af33cc77cc
Mikkel Malmberg <mikkel@brnbw.com>
2025-04-30 22:43:26 +0200

    Rubyfmt

--- test/satanic_pages_test.rb
+++ test/satanic_pages_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class SatanicPagesTest < ActiveSupport::TestCase
-  test "it has a version number" do
+  test("it has a version number") do
     assert SatanicPages::VERSION
   end
 end
