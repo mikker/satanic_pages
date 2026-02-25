dee260c4e95c74dd37317ad9fd55831c7e71759e
Mikkel Malmberg <mikkel@brnbw.com>
2026-02-25 12:18:52 +0100

    Fix Rails template annotation stripping for multiline

--- lib/satanic_pages/markdown_template_handler.rb
+++ lib/satanic_pages/markdown_template_handler.rb
@@ -3,13 +3,15 @@
 module SatanicPages
   class MarkdownTemplateHandler < MarkdownRails::Renderer::Rails
     include Redcarpet::Render::SmartyPants
+    TEMPLATE_ANNOTATION_REGEX =
+      /<!\p{Pd}{1,2}\s*(BEGIN|END)\s+.*?\s*\p{Pd}{1,2}>/m
 
     def preprocess(source)
       frontmatter, rest = Frontmatter.parse(source)
 
       render(inline: rest, handler: :erb, locals: {data: frontmatter})
         # Remove template comments (including SmartyPants converted dashes)
-        .gsub(/<!\p{Pd}{1,2}\s*(BEGIN|END)\s+.*?\s*\p{Pd}{1,2}>/, "")
+        .gsub(TEMPLATE_ANNOTATION_REGEX, "")
         # Force HTML tags to be inline
         .gsub(/<[^>]*?>/) { |tag| tag.gsub(/\n\s*/, " ") }
     end
